ec8ddd21c040efd74d4690fe27a79a469d44aed3
Pawan <pawanmaurya22660@gmail.com>
2023-08-24 16:57:45 +0530

    added commit

--- blogproject/settings.py
+++ blogproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-2w%ng5**(o(!mvi1fc_7fq%(smzcntnn2m!ok8au+$_#sz^sq5
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app','localhost',]
+ALLOWED_HOSTS = ['.vercel.app','127.0.0.1','localhost',]
 
 
 # Application definition
@@ -117,7 +117,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
